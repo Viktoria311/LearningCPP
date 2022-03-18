4606f37884ee4b19d542d4889fa926480669a7fe
Victoria <kotova-viktorija@mail.ru>
2022-03-19 02:21:45 +0300

    fix bugs/ but there are others

--- Prata_12/Prata_12_1/Prata_12_1/cow.cpp
+++ Prata_12/Prata_12_1/Prata_12_1/cow.cpp
@@ -6,8 +6,6 @@ Cow::Cow()
 {
 	name_ = "no name";
 	hobby_ = new char[20];
-	for (int i = 0; i <= 19; ++i)
-		hobby_[i] = '\0';
 	strcpy_s(hobby_, 19, "no hobby");
 	weight_ = 0.0;
 }
@@ -16,9 +14,7 @@ Cow::Cow(const std::string& name, const char* hobby, double weight)
 {
 	name_ = "no name";
 	hobby_ = new char[strlen(hobby) + 1];
-	for (int i = 0; i <= strlen(hobby); ++i)
-		hobby_[i] = '\0';
-	strcpy_s(hobby_, strlen(hobby) - 1, hobby);
+	strcpy_s(hobby_, strlen(hobby) + 1, hobby);
 	weight_ = weight;
 }
 
@@ -26,9 +22,7 @@ Cow::Cow(const Cow& c)
 {
 	name_ = c.name_;
 	hobby_ = new char[strlen(c.hobby_) + 1];
-	for (int i = 0; i <= strlen(c.hobby_); ++i)
-		hobby_[i] = '\0';
-	strcpy_s(hobby_, strlen(c.hobby_), c.hobby_);
+	strcpy_s(hobby_, strlen(c.hobby_) + 1, c.hobby_);
 	
 	weight_ = c.weight_;
 }
@@ -53,9 +47,8 @@ Cow& Cow::operator=(const Cow& c)
 		name_ = c.name_;
 		delete[] hobby_;
 		hobby_ = new char[strlen(c.hobby_ + 1)];
-		for (int i = 0; i <= strlen(c.hobby_); ++i)
-			hobby_[i] = '\0';
-		strcpy_s(hobby_, strlen(c.hobby_), c.hobby_);
+
+		strcpy_s(hobby_, strlen(c.hobby_) + 1, c.hobby_);
 		
 		weight_ = c.weight_;
 	}
--- Prata_12/Prata_12_1/Prata_12_1/main.cpp
+++ Prata_12/Prata_12_1/Prata_12_1/main.cpp
@@ -6,18 +6,21 @@ int main()
 {
 	std::vector<Cow> cows;
 
+	Cow some;
 	bool want;
 
 	do
 	{
-		Cow some;
-
 		std::cin >> some;
 		cows.push_back(some);
 		std::cout << "Do you want to add one more cow? 1 - yes, 0 - no ";
 		std::cin >> want;
+		//std::cin.get();
 		std::cin.ignore(100, '\n');
 	} while (want);
+
+	for (int i = 0; i < cows.size(); ++i)
+		std::cout << cows[i] << std::endl;
 
 	return 0;
 }
